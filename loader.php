238be84fbb782fdc69796bba8cd5114b0aba453e
mohammadreza <mrzkhalifeh8@gmail.com>
2018-05-31 09:50:54 +0430

    load common functions to memory

--- loader.php
+++ loader.php
@@ -2,4 +2,5 @@
 //start session
 session_start();
 //set default time zone to have correct time when converting time stamp
+date_default_timezone_set('timezone code');require_once(getcwd() .'/common.php');
 date_default_timezone_set('timezone code');
