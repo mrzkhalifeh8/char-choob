39270f701abd574ef3bfc605f28303d85d6bac42
mohammadreza <mrzkhalifeh8@gmail.com>
2018-06-02 09:14:10 +0430

    update

--- config.php
+++ config.php
@@ -3,4 +3,7 @@ global $config;
 $config['db']['host'] = 'localhost';
 $config['db']['user'] = 'usename';
 $config['db']['pass'] = 'password';
-$config['db']['name'] = 'database-name';$config['salt'] = "desired-hash";
+$config['db']['name'] = 'database-name';
+
+
+$config['salt'] = "desired-hash";
--- loader.php
+++ loader.php
@@ -2,7 +2,7 @@
 //start session
 session_start();
 //set default time zone to have correct time when converting time stamp
-date_default_timezone_set('timezone code');require_once(getcwd() .'/common.php');
 date_default_timezone_set('timezone code');
+require_once(getcwd() .'/common.php');
 require_once(getcwd() .'/config.php');
 require_once(getcwd() .'/locale/' . $config['lang'] .'.php');
